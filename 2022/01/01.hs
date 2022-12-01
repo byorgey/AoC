5d60c9ea7f70aaf7028cdb0b2e1d89adb7eaf2af
Brent Yorgey <byorgey@gmail.com>
2022-12-01 04:51:44 -0600

    2022 #1

---
+++ 2022/01/01.hs
@@ -0,0 +1,24 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-19.28 script --package containers --package split
+
+import           Control.Arrow   ((>>>))
+import           Data.List       (sortOn)
+import           Data.List.Split (splitOn)
+import           Data.Ord
+
+main = interact $
+  readInput >>> applyAll [solveA,solveB] >>> map show >>> unlines
+
+applyAll :: [a -> b] -> a -> [b]
+applyAll fs a = map ($ a) fs
+
+type Input = [[Int]]
+
+readInput :: String -> Input
+readInput = lines >>> splitOn [""] >>> map (map read)
+
+type Output = Int
+
+solveA, solveB :: Input -> Output
+solveA = map sum >>> maximum
+solveB = map sum >>> sortOn Down >>> take 3 >>> sum
