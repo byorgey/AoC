c261beb23740f5df2f949e60ed5f2b950d200584
Brent Yorgey <byorgey@gmail.com>
2024-12-05 14:49:49 -0600

    re-export new modules

--- 2024/AoC2024.lean
+++ 2024/AoC2024.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `AoC2024` library.
 -- Import modules here that should be built as part of the library.
 import AoC2024.Basic
+import AoC2024.Comb
+import AoC2024.V2
